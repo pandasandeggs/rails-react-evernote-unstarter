12d0c8687fd73411a62170b39f62dd957729a3db
pandasandeggs <theteatreehouse@gmail.com>
2018-10-10 14:45:46 -0500

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,15 +9,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello World!
           </p>
           <a
             className="App-link"
             href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
           >
-            Learn React
+            Thankfully
           </a>
         </header>
       </div>
